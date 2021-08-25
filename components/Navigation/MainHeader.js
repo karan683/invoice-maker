279a03b8edde07c4721c5f734d792614065fa3e8
Sujit <malisujit12@gmail.com>
2021-08-25 19:44:19 +0530

    Images added in public directory and navbar created absolute

--- components/Navigation/MainHeader.js
+++ components/Navigation/MainHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const MainHeader = (props) => {
   return (
-    <header className="w-full bg-white shadow-md h-22 fixed left-0 top-0 flex md:justify-between items-center px-0 py-4">
+    <header className="w-full bg-white shadow-md h-22 absolute left-0 top-0 flex md:justify-between items-center px-0 py-4">
       {props.children}
     </header>
   );
